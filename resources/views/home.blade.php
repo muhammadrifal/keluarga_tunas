42742d7d40b83cde9f52b46185ba454c0afe7ac0
Muhammad Rifal <rhifall.muhammad@gmail.com>
2026-02-06 00:47:35 +0700

    change favicon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Keluarga Tunas</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="icon" href="{{ asset('storage/img/favicon.png') }}" type="image/png">
     <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <style>
         body {
